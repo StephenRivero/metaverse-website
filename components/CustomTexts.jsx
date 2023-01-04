e4acca340790d6c38148130cbc95e6c3e752d943
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-01-05 05:25:40 +0800

    Add About Section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,22 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+
+import { textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles}) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((Letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {Letter === ' ' ? '\u00A0' : Letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({ title, textStyles}) => (
   <h2>Title Text</h2>
 );
