15ab5f8e6b96357f5a010e2824ca2d2cf7246caf
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-01-07 15:31:29 +0800

    Remove search icon in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => (
     className={`${styles.xPaddings} pt-8 relative`}
   >
     <div className='absolute w-[50%] inset-0 gradient-01' />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img 
+    <div className={`${styles.innerWidth} mx-auto flex justify-center gap-8 items-center`}>
+      {/* <img 
         src="/search.svg" 
         alt="search icon image"
         className='w-[24px] h-[24px] object-contain' 
-      />
-      <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
+      /> */}
+      <h2 className='font-extrabold text-[24px] leading-[30px] text-white mb-[2px]'>
         METAVERSUS
       </h2>
       <img 
         src="/menu.svg" 
         alt="menu icon" 
-        className='w-[24px] h-[24px] object-contain'
+        className='w-[24px] h-[24px] object-contain mt-[2px] ml-auto'
       />
     </div>
   </motion.nav>
