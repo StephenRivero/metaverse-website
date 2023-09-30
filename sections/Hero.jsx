faa07dc610ba39f72b9ad9e5a8db2e2d9045978e
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-09-30 10:49:56 +0800

    Fix eslint errors in Hero component

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,25 +1,27 @@
 'use client';
 
+/* eslint-disable jsx-quotes */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+
 import { motion } from 'framer-motion';
 
-import styles from '../styles'; 
-import { slideIn, staggerContainer, staggetContainer, textVariant } from '../utils/motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
-    <motion.div 
+    <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once: false, amount: 0.25}}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <div className='flex justify-center items-center flex-col relative z-10'>
-        <motion.h1 variants={textVariant(1.1)}
-        className={styles.heroHeading}>
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
           Metaverse
         </motion.h1>
-        <motion.div 
+        <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
@@ -33,17 +35,17 @@ const Hero = () => (
         variants={slideIn('right', 'tween', 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        <div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]' />
-        <img 
-          src="/cover.png" 
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+        <img
+          src="/cover.png"
           alt="cover image"
-          className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative' 
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
         />
         <a href="#explore">
           <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
-            <img 
-              src="/stamp.png" 
-              alt="stamp image" 
+            <img
+              src="/stamp.png"
+              alt="stamp image"
               className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
             />
           </div>
